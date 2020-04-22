07554706fd1adeebcb706fba92e9def0e8f1e6f6
a-lemonnier <40183590+a-lemonnier@users.noreply.github.com>
2020-04-22 13:41:53 +0200

    Update make_md.sh

--- make_md.sh
+++ make_md.sh
@@ -5,6 +5,8 @@ mv *.png png
 echo "# crappy-code-covid [![Build Status](https://cloud.drone.io/api/badges/a-lemonnier/crappy-code-covid/status.svg)](https://cloud.drone.io/a-lemonnier/crappy-code-covid) ![py CI](https://github.com/a-lemonnier/crappy-code-covid/workflows/py%20CI/badge.svg)" > README.md
 echo " " >> README.md
 echo "A bad py code fitting the covid data from ECDC. ECDC updates his database at 12h00 CEST atm." >> README.md
+echo " " >> README.md
+echo "Mise à jour quotidienne par action github." > README.md
 
 echo " " >> README.md
 
