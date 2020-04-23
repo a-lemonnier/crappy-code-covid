1474287de6cf86e9201e09e14e89021e901be511
a-lemonnier <audric.lemonnier@obspm.fr>
2020-04-23 14:27:10 +0200

    Update

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # crappy-code-covid [![Build Status](https://cloud.drone.io/api/badges/a-lemonnier/crappy-code-covid/status.svg)](https://cloud.drone.io/a-lemonnier/crappy-code-covid) ![py CI](https://github.com/a-lemonnier/crappy-code-covid/workflows/py%20CI/badge.svg)
  
-A bad py code fitting the covid data from ECDC. ECDC updates his database at 12h00 CEST atm.
+A bad py code fitting the covid data from ECDC. ECDC updates his database at 12h00 CEST atm. Demographic data are from World Bank.
+ 
+- [ECDC Data](https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide)
+- [WB Data](https://data.worldbank.org/indicator/sp.pop.totl)
+ 
  
 Mise à jour quotidienne automatique par action github.
  
--- make_md.sh
+++ make_md.sh
@@ -4,7 +4,15 @@ mv *.png png
 
 echo "# crappy-code-covid [![Build Status](https://cloud.drone.io/api/badges/a-lemonnier/crappy-code-covid/status.svg)](https://cloud.drone.io/a-lemonnier/crappy-code-covid) ![py CI](https://github.com/a-lemonnier/crappy-code-covid/workflows/py%20CI/badge.svg)" > README.md
 echo " " >> README.md
-echo "A bad py code fitting the covid data from ECDC. ECDC updates his database at 12h00 CEST atm." >> README.md
+echo "A bad py code fitting the covid data from ECDC. ECDC updates his database at 12h00 CEST atm. Demographic data are from World Bank." >> README.md
+
+echo " "  >> README.md
+
+echo  "- [ECDC Data](https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide)" >> README.md
+echo  "- [WB Data](https://data.worldbank.org/indicator/sp.pop.totl)" >> README.md
+
+echo " "  >> README.md
+
 echo " " >> README.md
 echo "Mise à jour quotidienne automatique par action github." >> README.md
 
